3e7717baf252c17ef631e7f28d2a49ce895ec7eb
Savita Rankavat <savita@Savitas-MacBook-Pro.local>
2023-04-05 17:21:21 -0500

    created contact form

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,11 +5,11 @@ export default function Portfolio(props) {
     return ( 
         <div>
         {props.projects.map((projects) => {
-        <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src={projects.img}></img>  
-        <div class="card-body">
-          <a href={projects.deployed} class="card-link">{projects.title}</a>
-          <a href={projects.github} class="card-link"><FontAwesomeIcon icon="fa-brands fa-github" /></a>
+        <div className="card" style="width: 18rem;">
+        <img className="card-img-top" src={projects.img}></img>  
+        <div className="card-body">
+          <a href={projects.deployed} className="card-link">{projects.title}</a>
+          <a href={projects.github} className="card-link"><FontAwesomeIcon icon="fa-brands fa-github" /></a>
         </div>
       </div>  
     })}
