049028029b8d9a9370a7550635040cf6927bdaf1
Savita Rankavat <savita@Savitas-MacBook-Pro.local>
2023-04-09 10:16:22 -0500

    styling

--- portfolio/src/components/pages/About.js
+++ portfolio/src/components/pages/About.js
@@ -5,11 +5,14 @@ import '../styles/About.css';
 export default function About() {
   return (
     <div>
-      <h1>About Me</h1>
+      <h1>ABOUT ME</h1>
       <img src={me} alt='' className='me'></img>
-      <p>
-        Welcome to my portfolio! I am an aspiring software developer. I currently work full time
-        in Quality Assurance as a Post Development Testing Coordinator. 
+      <p className='intro'>
+      Welcome to my portfolio! My name is Savita Manchanda, and I hold a Bachelor's degree in Mechanical Engineering. After graduation, I landed a full-time job in Quality Assurance as a Post-Development Testing Coordinator at SEI Investments.
+
+However, my interests didn't stop there. I've always been fascinated by the world of technology and software development. To further pursue this passion, I'm currently enrolled in a part-time Full Stack Web Development Bootcamp at Columbia University. Through this program, I've gained hands-on experience in coding languages such as HTML, CSS, JavaScript, and React, and I'm eager to keep learning and expanding my skillset.
+
+My experience in both mechanical engineering and software development has taught me how to approach complex problems with a holistic perspective. With a keen eye for detail and an eagerness to learn, I'm excited to tackle new challenges and make meaningful contributions to the tech industry. Thank you for taking the time to visit my portfolio, and I look forward to connecting with you soon.
       </p>
     </div>
   );
